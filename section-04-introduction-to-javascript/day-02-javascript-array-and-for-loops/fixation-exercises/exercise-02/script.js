4bbc82400857fb8eebf3dcd82df4bfbe44f1ebdd
vitoraok1 <vitoraoki17@gmail.com>
2022-09-25 12:23:17 -0300

    Using the for loop to print array results

---
+++ section-04-introduction-to-javascript/day-02-javascript-array-and-for-loops/fixation-exercises/exercise-02/script.js
@@ -0,0 +1,7 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+//Para imprimir no console cada elemento da array de forma mais prática utilizaremos a estrutura for de repetição:
+
+for (let index = 0; index < groceryList.length; index += 1){
+    console.log(groceryList[index]);
+}

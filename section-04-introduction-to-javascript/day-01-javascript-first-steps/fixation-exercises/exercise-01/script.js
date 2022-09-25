1a800c3b2bfd4c83a6e4f4ad95781d09def574e9
vitoraok1 <vitoraoki17@gmail.com>
2022-09-25 09:11:22 -0300

    Creation of constants, variable and printing of results

---
+++ section-04-introduction-to-javascript/day-01-javascript-first-steps/fixation-exercises/exercise-01/script.js
@@ -0,0 +1,11 @@
+const myName = "Vitor Aoki";
+const birthCity = "Votuporanga-SP";
+let birthYear = 1994;
+birthYear = 2030;
+birthCity = "Porto Velho-RO"
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//O Console irá apresentar um erro pois tentamos alterar o valor de uma constante, no caso birthCity.

3a54a9cece6940995b3aa73caa59ce0654c94443
vitoraok1 <vitoraoki17@gmail.com>
2022-09-27 10:32:03 -0300

    Creating arithmetic functions

---
+++ section-04-introduction-to-javascript/day-04-javascript-objects-and-functions/fixation-exercises/exercise-02/script.js
@@ -0,0 +1,29 @@
+//Função de soma
+
+function sum(a, b) {
+    return a + b;
+};
+
+//Função de subtração
+
+function subtraction(a, b) {
+    return a - b;
+};
+
+//Função de multiplicação
+
+function multiplication(a, b) {
+    return a * b;
+};
+
+//Função de divisão
+
+function division(a, b) {
+    return a / b;
+};
+
+//Função de módulo
+
+function module(a, b) {
+    return a % b;
+};

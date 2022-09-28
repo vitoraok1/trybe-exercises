96dcdc8bb0a238acd7e1834e680f9288d89dde25
vitoraok1 <vitoraoki17@gmail.com>
2022-09-28 18:57:49 -0300

    Changing section background colors using getElementsByClassName

--- section-05-javascript-dom-events-and-web-storage/day-01-javascript-dom-and-selectors/fixation-exercises/exercise-03/script.js
+++ section-05-javascript-dom-events-and-web-storage/day-01-javascript-dom-and-selectors/fixation-exercises/exercise-03/script.js
@@ -1,2 +1,8 @@
 const title = document.getElementsByTagName('header');
 title[0].style.background = '#04b36d';
+
+const section = document.getElementsByClassName('emergency-tasks');
+section[0].style.background = '#ff9f84';
+
+const section2 = document.getElementsByClassName('no-emergency-tasks');
+section2[0].style.background = '#f9db5e';

6f1ea7764deeb750c399f5eaeeeb3354eddf4136
vitoraok1 <vitoraoki17@gmail.com>
2022-09-29 18:33:59 -0300

    Rescuing, creating and adding a son to parent element

--- section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-02/script.js
+++ section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-02/script.js
@@ -4,3 +4,10 @@ const pai = document.getElementById('pai');
 const irmaoElementoOndeVoceEsta = document.createElement('section');
 irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
 pai.appendChild(irmaoElementoOndeVoceEsta);
+
+// Create a son to elementoOndeVoceEsta.
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);

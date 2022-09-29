60fad433598caa2640e82e246a6dda583c5243f8
vitoraok1 <vitoraoki17@gmail.com>
2022-09-29 18:30:05 -0300

    Rescuing, creating and adding an element to a parent

---
+++ section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-02/script.js
@@ -0,0 +1,6 @@
+// Create a brother to elementoOndeVoceEsta.
+
+const pai = document.getElementById('pai');
+const irmaoElementoOndeVoceEsta = document.createElement('section');
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);

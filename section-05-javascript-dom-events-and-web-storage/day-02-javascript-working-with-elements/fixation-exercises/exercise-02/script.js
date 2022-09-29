7fe96f3e5b6b9ecdc1fbf613991d5ad6e41226cf
vitoraok1 <vitoraoki17@gmail.com>
2022-09-29 18:45:33 -0300

    Using parentElement and nextElementSibling to access a element

--- section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-02/script.js
+++ section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-02/script.js
@@ -18,3 +18,11 @@ const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
 const filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
 filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
 primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+// From this created child, access terceiroFilho.
+
+const terceiroFilho = filhoDoPrimeiroFilhoDoFilho
+    .parentElement
+    .parentElement
+    .nextElementSibling;
+console.log(terceiroFilho);

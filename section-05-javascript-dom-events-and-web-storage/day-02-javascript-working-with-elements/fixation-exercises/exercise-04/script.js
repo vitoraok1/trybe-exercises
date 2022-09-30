dcb3dd92c91ba9be674c249ce65f87f446284b77
vitoraok1 <vitoraoki17@gmail.com>
2022-09-30 15:46:05 -0300

    Creating the main tag element and using appendChild to embed it in the html

--- section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-04/script.js
+++ section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-04/script.js
@@ -1,5 +1,11 @@
-//Add the `h1` tag with the text `Exercício - JavaScript DOM` as child of `body` tag.
+// Add the `h1` tag with the text `Exercício - JavaScript DOM` as child of `body` tag.
 
 const tagH1 = document.createElement('h1');
 tagH1.innerText = 'Exercício - JavaScript DOM';
 document.body.appendChild(tagH1);
+
+// Add `main` tag with class `main-content` as child of `body` tag;
+
+const mainTag = document.createElement('main');
+mainTag.className = 'main-content';
+document.body.appendChild(mainTag);

d404a0cc3e8375e419aa188e4f189b666f9fdb51
vitoraok1 <vitoraoki17@gmail.com>
2022-09-30 15:42:15 -0300

    Creating an element and using appendChild to embed it in html

---
+++ section-05-javascript-dom-events-and-web-storage/day-02-javascript-working-with-elements/fixation-exercises/exercise-04/script.js
@@ -0,0 +1,5 @@
+//Add the `h1` tag with the text `Exercício - JavaScript DOM` as child of `body` tag.
+
+const tagH1 = document.createElement('h1');
+tagH1.innerText = 'Exercício - JavaScript DOM';
+document.body.appendChild(tagH1);

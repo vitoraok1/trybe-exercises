7b2834c8d0bd9fa57b019146ba7456a6198dd79d
vitoraok1 <vitoraoki17@gmail.com>
2022-10-03 08:06:58 -0300

    Using for, if condition, create element and appendChild to add calendar days

--- section-05-javascript-dom-events-and-web-storage/day-03-javascript-events/fixation-exercises/exercise-01/script.js
+++ section-05-javascript-dom-events-and-web-storage/day-03-javascript-events/fixation-exercises/exercise-01/script.js
@@ -13,5 +13,35 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  // Part 1
+
+  window.onload = function createMonthDays () {
+    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const days = document.getElementById('days');
+
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+        let decemberDays = decemberDaysList[index];
+
+        let dayListItem = document.createElement('li');
+        dayListItem.innerHTML = decemberDays;
+        dayListItem.className = 'day';
+
+        if (decemberDays === 24 || decemberDays === 25 | decemberDays === 31) {
+          dayListItem.classList.add('holiday');
+        }
+
+        if (decemberDays === 4 || decemberDays === 11 || decemberDays === 18 || decemberDays === 25) {
+          dayListItem.classList.add('friday');
+        }
+
+        days.appendChild(dayListItem);
+    }
+
+
+
+
+
+
+
+  }
   

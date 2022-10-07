d05549e846260d1e32eee0233c852b59b5afcc54
vitoraok1 <vitoraoki17@gmail.com>
2022-10-07 09:30:23 -0300

    Refactoring

--- section-06-html-and-css-forms-flexbox-and-responsive/day-01-html-and-css-forms/fixation-exercises/exercise-03/script.js
+++ section-06-html-and-css-forms-flexbox-and-responsive/day-01-html-and-css-forms/fixation-exercises/exercise-03/script.js
@@ -1,5 +1,26 @@
-const sendButton = document.getElementById('send-button');
+window.onload = function () {
+    const clearButton = document.getElementById('clear-button')
+    clearButton.addEventListener('click', clearForm);
+    const sendButton = document.getElementById('send-button');
+    sendButton.addEventListener('click', keepTexts);
+};
 
-sendButton.addEventListener('click', function(event){
+//Função para manter os dados preenchidos ao enviar o formulário
+
+function keepTexts(event) {
     event.preventDefault();
-})
+}
+
+// Função para criar um botão que reseta o formulário (outra opção seria fazer direto no html utilizando o input com a propriedade "reset")
+
+function clearForm() {
+const allInputs = document.querySelectorAll('input');
+const textArea = document.getElementById('textarea')
+
+for (let index = 0; index < allInputs.length; index += 1) {
+    const userInput = allInputs[index];
+    userInput.value = '';
+    userInput.checked = false;
+    }
+textArea.value = '';
+}

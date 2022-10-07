6cd2ee8d279165c4efcf1e6bce4116ab394c4d30
vitoraok1 <vitoraoki17@gmail.com>
2022-10-07 09:46:10 -0300

    Creating the function to validate if the amount of characters is correct and issue an alert

--- section-06-html-and-css-forms-flexbox-and-responsive/day-01-html-and-css-forms/fixation-exercises/exercise-03/script.js
+++ section-06-html-and-css-forms-flexbox-and-responsive/day-01-html-and-css-forms/fixation-exercises/exercise-03/script.js
@@ -11,6 +11,12 @@ window.onload = function () {
 
 function keepTexts(event) {
     event.preventDefault();
+    const validation = textInputValidation();
+    if (validation === false) {
+      alert('Dados inválidos');
+    } else {
+      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
 }
 
 // Função para criar um botão que reseta o formulário (outra opção seria fazer direto no html utilizando o input com a propriedade "reset")
@@ -35,4 +41,22 @@ function enableSubmit() {
     sendButton.disabled = !terms.checked;
   }
 
-//
+// Função para verificar se os inputs possuem o mínimo/máximo de caracteres exigidos para podermos depois convocá-lo na função que contem o preventDefault e emitirmos o alerta caso os dados sejam inválidos ou caso esteja tudo certo.
+
+function textInputValidation() {
+    const email = document.getElementById('mail').value.length;
+    const invalidEmail = email < 10 || email > 50;
+  
+    const name = document.getElementById('name').value.length;
+    const invalidName = name < 10 || name > 40;
+  
+    const reason = document.getElementById('textarea').value.length;
+    const invalidReason = reason > 500;
+  
+    if (invalidEmail || invalidName || invalidReason) {
+      return false;
+    }
+    else {
+      return true;
+    }
+}

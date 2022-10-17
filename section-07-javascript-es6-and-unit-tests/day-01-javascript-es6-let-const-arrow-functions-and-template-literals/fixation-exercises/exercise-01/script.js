1bb026f6cdd9eeb0fa1bc0ed6e28badec0f680e2
vitoraok1 <vitoraoki17@gmail.com>
2022-10-17 15:29:08 -0300

    Modifying concatenations for template literals

--- section-07-javascript-es6-and-unit-tests/day-01-javascript-es6-let-const-arrow-functions-and-template-literals/fixation-exercises/exercise-01/script.js
+++ section-07-javascript-es6-and-unit-tests/day-01-javascript-es6-let-const-arrow-functions-and-template-literals/fixation-exercises/exercise-01/script.js
@@ -1,13 +1,13 @@
 const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
       const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
       console.log(elseScope);
     }
-    
+
   }
 
   testingScope(true);

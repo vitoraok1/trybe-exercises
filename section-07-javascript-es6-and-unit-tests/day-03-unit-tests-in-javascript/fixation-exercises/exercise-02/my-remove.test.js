8df4fdb7e26ab24f64ff1a79d1803f3286292d1b
vitoraok1 <vitoraoki17@gmail.com>
2022-10-19 19:28:20 -0300

    Creating the third test to function myRemove

--- section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-02/my-remove.test.js
+++ section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-02/my-remove.test.js
@@ -8,4 +8,8 @@ describe('Testa a função myRemove', () => {
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });

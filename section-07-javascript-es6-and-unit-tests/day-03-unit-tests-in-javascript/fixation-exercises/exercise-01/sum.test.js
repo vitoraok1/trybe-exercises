0f15f627b2a792a9a001f8a01719679c7ca2f2ea
vitoraok1 <vitoraoki17@gmail.com>
2022-10-19 18:58:53 -0300

    Creating the second test for function sum

--- section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-01/sum.test.js
+++ section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-01/sum.test.js
@@ -1,7 +1,11 @@
 const sum = require(`./sum`);
 
 describe('Teste se o retorno de sum(4, 5) é 9', () => {
-  it('Deve retornar o resultado da soma', () => {
+  it('Deve retornar o resultado da soma de 4 + 5 que é igual a 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
+
+  it('Deve retornar o resultado da soma de 0 + 0 que é igual a 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  })
 });

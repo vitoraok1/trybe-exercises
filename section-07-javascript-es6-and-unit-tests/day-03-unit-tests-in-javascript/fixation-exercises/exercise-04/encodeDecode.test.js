2d4731c702b2a647c4a77190802016a46b41c502
vitoraok1 <vitoraoki17@gmail.com>
2022-10-20 10:02:59 -0300

    Creating the second test to check if the vowels are replaced by the respective numbers

--- section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-04/encodeDecode.test.js
+++ section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-04/encodeDecode.test.js
@@ -8,4 +8,9 @@ describe('Testa as funções encode e decode', () => {
   it('Testa se decode é realmente uma função', () => {
     expect(typeof decode).toEqual('function');
   });
+
+  it('Testa se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5 na função encode', () => {
+    expect(encode('Eu vou ser aprovado no projeto de hoje!')).toEqual('E5 v45 s2r 1pr4v1d4 n4 pr4j2t4 d2 h4j2!');
+    expect(encode('projeto unit tests')).toEqual('pr4j2t4 5n3t t2sts');
+  });
 });

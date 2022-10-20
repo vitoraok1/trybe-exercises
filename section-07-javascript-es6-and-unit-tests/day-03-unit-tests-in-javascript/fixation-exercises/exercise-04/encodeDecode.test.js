5c38aba6f2d736626b72d9f282112e8abaaf549e
vitoraok1 <vitoraoki17@gmail.com>
2022-10-20 09:54:48 -0300

    Creating the first test to check if encode/decode are functions

--- section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-04/encodeDecode.test.js
+++ section-07-javascript-es6-and-unit-tests/day-03-unit-tests-in-javascript/fixation-exercises/exercise-04/encodeDecode.test.js
@@ -1,1 +1,11 @@
 const { encode, decode } = require('./encodeDecode');
+
+describe('Testa as funções encode e decode', () => {
+  it('Testa se encode é realmente uma função', () => {
+    expect(typeof encode).toEqual('function');
+  });
+
+  it('Testa se decode é realmente uma função', () => {
+    expect(typeof decode).toEqual('function');
+  });
+});

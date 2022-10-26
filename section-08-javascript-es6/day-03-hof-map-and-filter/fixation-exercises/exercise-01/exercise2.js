bb52a5d5aebc6ad7f42e10a0ef3b622a8b687c86
vitoraok1 <vitoraoki17@gmail.com>
2022-10-26 14:26:31 -0300

    Using map and sort method to return all authors and their ages in the year the book was released

--- section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise2.js
+++ section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise2.js
@@ -1,5 +1,13 @@
 const books = require('./books');
 
-function nameAndAge() {
-  // escreva seu código aqui
-}
+const nameAndAge = (booksArray) => {
+  const arrayBooks = booksArray.map((book) => (
+    {
+    author:  book.author.name,
+    age: book.releaseYear - book.author.birthYear,
+    }
+  ));
+  return arrayBooks.sort((a, b) => a.age - b.age);
+};
+
+console.log(nameAndAge(books));

584c99961326561f2c7010f161992650c069c8be
vitoraok1 <vitoraoki17@gmail.com>
2022-10-26 14:50:07 -0300

    Using filter and sort method to return a array containing books over 60 years since their publication

--- section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise4.js
+++ section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise4.js
@@ -1,5 +1,11 @@
 const books = require('./books');
 
-const oldBooksOrdered = () => {
-  // escreva seu código aqui
+const oldBooksOrdered = (booksArray) => {
+  const actualYear = new Date().getFullYear();
+  
+  return booksArray.filter((book) => (
+    actualYear - book.releaseYear >= 60))
+    .sort((a, b) => a.releaseYear - b.releaseYear);
 };
+
+console.log(oldBooksOrdered(books));

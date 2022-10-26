2004be5935f65c444c4e7a89d6da65a2af732af4
vitoraok1 <vitoraoki17@gmail.com>
2022-10-26 15:40:06 -0300

    Using find method to return the book written by the author that starts with 3 initials the name

--- section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise7.js
+++ section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise7.js
@@ -1,7 +1,11 @@
 const books = require('./books');
 
 const authorWith3DotsOnName = (booksArray) => {
-  // escreva seu código aqui
+  return booksArray.find((book) => (
+      book.author.name[1] === '.' &&
+      book.author.name[4] === '.' &&
+      book.author.name[7] === '.'
+    )).name;
 }
 
 console.log(authorWith3DotsOnName(books));

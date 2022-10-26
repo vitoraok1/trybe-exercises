1f12cc33482d596f9589a565d67d538873993aea
vitoraok1 <vitoraoki17@gmail.com>
2022-10-26 15:17:16 -0300

    Using filter and map method to return only the author names of certain genres

--- section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise5.js
+++ section-08-javascript-es6/day-03-hof-map-and-filter/fixation-exercises/exercise-01/exercise5.js
@@ -1,5 +1,9 @@
 const books = require('./books');
 
-const fantasyOrScienceFictionAuthors = () => {
-  // escreva seu código aqui
+const fantasyOrScienceFictionAuthors = (booksArray) => {
+  return booksArray
+    .filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia')
+    .map((book) => book.author.name).sort();
 };
+
+console.log(fantasyOrScienceFictionAuthors(books));

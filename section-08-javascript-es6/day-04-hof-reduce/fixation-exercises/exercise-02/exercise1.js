df77c1a878a07d91d60bc7c7b06b5885777758ff
vitoraok1 <vitoraoki17@gmail.com>
2022-10-27 18:36:44 -0300

    Using reduce method to return the average age of authors when books were published

--- section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-02/exercise1.js
+++ section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-02/exercise1.js
@@ -3,7 +3,7 @@ const books = require('./books');
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
 function reduceNames() {
-  return books.reduce((acc, curr) => `${acc} ${curr.author.name},`, '')
+  return books.reduce((acc, curr) => `${acc} ${curr.author.name},`, '',)
 }
 
 console.log(reduceNames());
--- section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-02/exercise2.js
+++ section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-02/exercise2.js
@@ -3,5 +3,11 @@ const books = require('./books');
 const expectedResult = 43;
 
 function averageAge() {
-  // escreva seu código aqui
+  const releasedBooks = books.length;
+  const sumOfAges = books.reduce((acc, curr) => (
+    acc + (curr.releaseYear - curr.author.birthYear)
+  ), 0);
+  return sumOfAges / releasedBooks;
 }
+
+console.log(averageAge());

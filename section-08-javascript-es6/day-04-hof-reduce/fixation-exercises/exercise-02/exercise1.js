a70a2bccd3ac963762fc0118164a46300aa65a95
vitoraok1 <vitoraoki17@gmail.com>
2022-10-27 15:59:22 -0300

    Using reduce method to creat a string that contains all author names

--- section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-02/exercise1.js
+++ section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-02/exercise1.js
@@ -3,5 +3,7 @@ const books = require('./books');
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
 function reduceNames() {
-  // escreva seu código aqui
+  return books.reduce((acc, curr) => `${acc} ${curr.author.name},`, '')
 }
+
+console.log(reduceNames());

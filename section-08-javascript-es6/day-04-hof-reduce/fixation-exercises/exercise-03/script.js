dcf61ba2d5a2724e8d6d15847cff900777a00cce
vitoraok1 <vitoraoki17@gmail.com>
2022-10-27 19:24:31 -0300

    Using forEach and reduce method to return return the number of times a letter is repeated

--- section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-03/script.js
+++ section-08-javascript-es6/day-04-hof-reduce/fixation-exercises/exercise-03/script.js
@@ -7,5 +7,17 @@ const names = [
 const expectedResult = 20;
 
 function containsA() {
-  // escreva seu código aqui
+  let wordSum = 0;
+  names.forEach((name) => {
+    const letters = name.split('');
+    wordSum += letters.reduce((sum, letter) => {
+      if (letter.toLowerCase() === 'a') {
+        return sum + 1;
+      }
+      return sum;
+    }, 0);
+  });
+  return wordSum;
 };
+
+console.log(containsA());

29918cc4e5159b9bb8c20e03251e09dd5d6f401d
vitoraok1 <vitoraoki17@gmail.com>
2022-10-28 10:21:50 -0300

    Creating the base script.js file from exercise 02

---
+++ section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-02/script.js
@@ -0,0 +1,11 @@
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};

21fbdf8883c2fbd3bd28ecbac56d6ecbb2d3f580
vitoraok1 <vitoraoki17@gmail.com>
2022-10-28 09:59:29 -0300

    Using spread operator to return a single list

--- section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-01/script.js
+++ section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-01/script.js
@@ -1,11 +1,11 @@
 // Faça uma lista com as suas frutas favoritas
-const specialFruit = ['', '', ''];
+const specialFruit = ['Mamão', 'Melancia', 'Melão'];
 
 // Faça uma lista de complementos que você gostaria de adicionar
-const additionalItens = ['', '', ''];
+const additionalItens = ['Maça', 'Banana', 'Ameixa'];
 
 const fruitSalad = (fruit, additional) => {
-  // Escreva sua função aqui
+  return [...fruit, ...additional];
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));

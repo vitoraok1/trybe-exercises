f2110b1be7fd5c0170333f090084ad828f9b90ff
vitoraok1 <vitoraoki17@gmail.com>
2022-10-28 10:45:40 -0300

    Using array destructuring and spread operator to solve the requirements

--- section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-03/script.js
+++ section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-03/script.js
@@ -2,18 +2,22 @@ const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
 saudacoes[1](saudacoes[0]); // Olá
 
-// Produza o mesmo resultado acima, porém utilizando array destructuring
+const [saudacao] = saudacoes;
+console.log(saudacao);
+
+// -----------------------------------------------------------------------------------------
 
 let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
 
-console.log(comida, animal, bebida); // arroz gato água
+[comida, animal, bebida] = [bebida, comida, animal];
+console.log(comida, animal, bebida);
 
-// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+// -----------------------------------------------------------------------------------------
 
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-console.log(numerosPares); // [6, 8, 10, 12];
+[,,, ...numerosPares] = numerosPares
 
-// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+console.log(numerosPares);

759bdd82e8df7945feb30b4c1df99da191479043
vitoraok1 <vitoraoki17@gmail.com>
2022-10-28 17:01:37 -0300

    Using array destructuring to return an object with the car information

--- section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-12/script.js
+++ section-08-javascript-es6/day-05-spread-operator-rest-destructuring-and-more/fixation-exercises/exercise-12/script.js
@@ -3,3 +3,14 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+
+const toObject = (array) => {
+  const [carName, brandName, yearModel] = array;
+  return {
+    name: carName,
+    brand: brandName,
+    year: yearModel,
+  }
+}
+
+console.log(toObject(chiron));

1951b5388c4ecfb50adc31c0f8546f1331a2a6b8
vitoraok1 <vitoraoki17@gmail.com>
2022-11-09 10:35:02 -0300

    Adding an eventlistener to implement a new function to exercise 01

--- section-01-javascript-and-asynchronous-tests/day-01-development-environment/fixation-exercises/exercise-01/src/main.js
+++ section-01-javascript-and-asynchronous-tests/day-01-development-environment/fixation-exercises/exercise-01/src/main.js
@@ -1,4 +1,6 @@
 import { nanoid } from 'nanoid'
+import copy from 'clipboard-copy';
+
 import "./style.css"
 
 const passwordBtnEl = document.querySelector('button');
@@ -7,4 +9,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
 });

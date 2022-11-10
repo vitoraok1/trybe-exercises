9d1d992fdb6c1f0d7c562647fb69c3659c63fe4b
vitoraok1 <vitoraoki17@gmail.com>
2022-11-10 09:00:43 -0300

    Retrieving html elements and adding an event listener for the validation button

--- section-01-javascript-and-asynchronous-tests/day-01-development-environment/fixation-exercises/exercise-02/src/main.js
+++ section-01-javascript-and-asynchronous-tests/day-01-development-environment/fixation-exercises/exercise-02/src/main.js
@@ -1,1 +1,8 @@
 import validator from 'validator';
+
+const textField = document.getElementById('value');
+const validateBtn = document.getElementById('button');
+const selector = document.getElementById('option');
+const outputText = document.getElementById('answer');
+
+validateBtn.addEventListener('click', (event) => event.preventDefault());

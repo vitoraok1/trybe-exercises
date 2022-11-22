aac502c46a0d03513d20d065c34c874eada0c940
vitoraok1 <vitoraoki17@gmail.com>
2022-11-22 15:33:58 -0300

    Creating a skill list and running the application

--- section-02-introduction-to-react/day-01-hello-world-on-react/fixation-exercises/exercise-01/my-app/src/About.js
+++ section-02-introduction-to-react/day-01-hello-world-on-react/fixation-exercises/exercise-01/my-app/src/About.js
@@ -5,12 +5,26 @@ class MyApp extends React.Component {
   render() {
     return (
       <div>
-        <h1 className='MyApp'>
+        <h1 className='name-title'>
           Vitor Aoki
         </h1>
         <p>
           Paranaense, 28 anos, Estudante de Programação
         </p>
+        <h2>
+          Minhas Habilidades
+        </h2>
+        <ul className='skills-list'>
+          <li>
+            Proatividade
+          </li>
+          <li>
+            Comunicação
+          </li>
+          <li>
+            Vontade de aprender
+          </li>
+        </ul>
       </div>
     );
   }

f2226737a9265f0ac6c796ee4f901465e606a44b
vitoraok1 <vitoraoki17@gmail.com>
2022-11-22 19:01:21 -0300

    Structuring the image component call

--- section-02-introduction-to-react/day-02-react-components/fixation-exercises/exercise-01/exercise-image-component/src/App.js
+++ section-02-introduction-to-react/day-02-react-components/fixation-exercises/exercise-01/exercise-image-component/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Image from './Image';
 
 function App() {
+  const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Image source={ URL } alternativeText="Cute cat staring"/>
+    </section>
   );
 }
 

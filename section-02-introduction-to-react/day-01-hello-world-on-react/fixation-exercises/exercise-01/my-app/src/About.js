882a297e3d77a0aae67df0827872ecd7c04f9004
vitoraok1 <vitoraoki17@gmail.com>
2022-11-22 15:28:37 -0300

    Creating a new React project and executing the first steps of exercise 01

---
+++ section-02-introduction-to-react/day-01-hello-world-on-react/fixation-exercises/exercise-01/my-app/src/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class MyApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className='MyApp'>
+          Vitor Aoki
+        </h1>
+        <p>
+          Paranaense, 28 anos, Estudante de Programação
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MyApp;

f12ce223d78fcc405be7d1b69456db6404e13556
vitoraok1 <vitoraoki17@gmail.com>
2022-11-29 11:18:58 -0300

    Changing the buttons so that each click increases the count by 1

--- section-03-components-events-and-forms-with-react/day-01-components-with-states-and-events/fixation-exercises/exercise-01/fancy-buttons/src/App.js
+++ section-03-components-events-and-forms-with-react/day-01-components-with-states-and-events/fixation-exercises/exercise-01/fancy-buttons/src/App.js
@@ -4,30 +4,42 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
-    this.clickButton1 = this.clickButton1.bind(this);
+    this.clickButton2 = this.clickButton2.bind(this);
+    this.clickButton3 = this.clickButton3.bind(this);
+
+    this.state = {
+      clickBtn1: 0,
+      clickBtn2: 0,
+      clickBtn3: 0,
+    }
+  }
+
+  clickButton1 = () => {
+    this.setState((previousState, props_) => ({
+      clickBtn1: previousState.clickBtn1 + 1
+    }));
   }
   
-  clickButton1 () {
-    console.log('Test log one');
-    console.log(this);
+  clickButton2 () {
+    this.setState((previousState, props_) => ({
+      clickBtn2: previousState.clickBtn2 + 1
+    }));
   }
   
-  clickButton2 = () => {
-    console.log('Test log two');
-    console.log(this);
-  }
-  
-  clickButton3 = () => {
-    console.log('Test log three');
-    console.log(this);
+  clickButton3 () {
+    this.setState((previousState, props_) => ({
+      clickBtn3: previousState.clickBtn3 + 1
+    }));
   }
 
   render() {
+    const { clickBtn1, clickBtn2, clickBtn3 } = this.state;
+
     return (
       <div>
-        <button onClick={this.clickButton1}>Test Button One</button>
-        <button onClick={this.clickButton2}>Test Button Two</button>
-        <button onClick={this.clickButton3}>Test Button Three</button>
+        <button onClick={ this.clickButton1 }>{ clickBtn1 }</button>
+        <button onClick={ this.clickButton2 }>{ clickBtn2 }</button>
+        <button onClick={ this.clickButton3 }>{ clickBtn3 }</button>
       </div>
     );
   }

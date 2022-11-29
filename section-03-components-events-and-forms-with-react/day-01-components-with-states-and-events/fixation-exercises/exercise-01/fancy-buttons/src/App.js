62eafd9dd36351b468f63c2b0db22b30e716baca
vitoraok1 <vitoraoki17@gmail.com>
2022-11-29 10:29:25 -0300

    Creating a button associated with a function that returns a log

--- section-03-components-events-and-forms-with-react/day-01-components-with-states-and-events/fixation-exercises/exercise-01/fancy-buttons/src/App.js
+++ section-03-components-events-and-forms-with-react/day-01-components-with-states-and-events/fixation-exercises/exercise-01/fancy-buttons/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return(
+function clickButton() {
+  console.log('Test log');
+}
 
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={clickButton}>Test Button</button>
+      </div>
+    );
+  }
 }
 
 export default App;

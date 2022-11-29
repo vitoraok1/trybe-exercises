6c7439891b4ea16ba868f4b8c33d66b375e0520f
vitoraok1 <vitoraoki17@gmail.com>
2022-11-29 10:33:09 -0300

    Update button names

--- section-03-components-events-and-forms-with-react/day-01-components-with-states-and-events/fixation-exercises/exercise-01/fancy-buttons/src/App.js
+++ section-03-components-events-and-forms-with-react/day-01-components-with-states-and-events/fixation-exercises/exercise-01/fancy-buttons/src/App.js
@@ -17,9 +17,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={clickButton1}>Test Button1</button>
-        <button onClick={clickButton2}>Test Button2</button>
-        <button onClick={clickButton3}>Test Button3</button>
+        <button onClick={clickButton1}>Test Button One</button>
+        <button onClick={clickButton2}>Test Button Two</button>
+        <button onClick={clickButton3}>Test Button Three</button>
       </div>
     );
   }

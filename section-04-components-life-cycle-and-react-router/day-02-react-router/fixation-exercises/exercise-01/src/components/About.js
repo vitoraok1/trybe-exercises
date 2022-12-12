65177a451ea90f930654d84bb0068edd2b9d84a9
vitoraok1 <vitoraoki17@gmail.com>
2022-12-12 10:23:26 -0300

    Adding links to the appropriate paths

--- section-04-components-life-cycle-and-react-router/day-02-react-router/fixation-exercises/exercise-01/src/components/About.js
+++ section-04-components-life-cycle-and-react-router/day-02-react-router/fixation-exercises/exercise-01/src/components/About.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class About extends Component {
   render() {
     return (
       <div>
         <h2>About</h2>
+        <Link to="/">Home</Link>
         <p> My awesome About component </p>
       </div>
     );
--- section-04-components-life-cycle-and-react-router/day-02-react-router/fixation-exercises/exercise-01/src/components/Users.js
+++ section-04-components-life-cycle-and-react-router/day-02-react-router/fixation-exercises/exercise-01/src/components/Users.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Users extends Component {
   render() {
     return (
       <div>
         <h2>Users</h2>
+        <Link to="/">Home</Link>
         <p> My awesome Users component </p>
       </div>
     );

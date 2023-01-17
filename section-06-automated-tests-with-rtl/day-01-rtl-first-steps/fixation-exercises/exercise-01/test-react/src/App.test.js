9f814a32e0b289ffb06ba6a04aaf79adc6ce0461
vitoraok1 <vitoraoki17@gmail.com>
2023-01-17 15:22:18 -0300

    adding info from exercise 01

--- section-06-automated-tests-with-rtl/day-01-rtl-first-steps/fixation-exercises/exercise-01/test-react/src/App.test.js
+++ section-06-automated-tests-with-rtl/day-01-rtl-first-steps/fixation-exercises/exercise-01/test-react/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Tela de inserção de email', () => {
+  it('Verificando se existe o campo Email.', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
+  
+  it('Verificando se existem dois botões', () => {
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
+  
+  it('Verificando se existe um botão de enviar', () => {
+    render(<App />);
+    const btnSend = screen.getByTestId('id-send');
+    expect(btnSend).toBeInTheDocument();
+    expect(btnSend).toHaveProperty('type', 'button');
+    expect(btnSend).toHaveValue('Enviar');
+  });
 });

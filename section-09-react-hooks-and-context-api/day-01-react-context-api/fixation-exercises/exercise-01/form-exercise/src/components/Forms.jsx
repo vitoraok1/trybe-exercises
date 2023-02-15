23e0ce24ce823a30d33f8dad923e4c155474bafe
vitoraok1 <vitoraoki17@gmail.com>
2023-02-15 16:05:31 -0300

    adding the inputs from form and css

--- section-09-react-hooks-and-context-api/day-01-react-context-api/fixation-exercises/exercise-01/form-exercise/src/components/Forms.jsx
+++ section-09-react-hooks-and-context-api/day-01-react-context-api/fixation-exercises/exercise-01/form-exercise/src/components/Forms.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import './Forms.css';
 
 export default function Forms() {
   return (
-    <div>Forms</div>
+    <section>
+      <h1>Personal student form</h1>
+      <div className="form-container">
+        <form className="form-inputs">
+          <input type="text" placeholder="Nome completo"/>
+          <input type="number" placeholder="Idade"/>
+          <input type="text" placeholder="Cidade"/>
+          <div className="radio-container">
+            <h4>
+              Qual módulo está estudando?
+            </h4>
+            <div>
+              <input type="radio" id="fundamentos" name="module" value="fundamentos"/>
+              <label htmlFor="fundamentos">Fundamentos</label>
+            </div>
+            <div>
+              <input type="radio" id="front-end" name="module" value="front-end" />
+              <label htmlFor="front-end">Front-End</label>
+            </div>
+            <div>
+              <input type="radio" id="back-end" name="module" value="back-end" />
+              <label htmlFor="back-end">Back-End</label>
+            </div>
+            <div>
+              <input type="radio" id="ciencias" name="module" value="ciencias" />
+              <label htmlFor="ciencias">Ciências da Computação</label>
+            </div>
+          </div>
+          <button type="button">Enviar</button>
+        </form>
+      </div>
+    </section>
   )
 }

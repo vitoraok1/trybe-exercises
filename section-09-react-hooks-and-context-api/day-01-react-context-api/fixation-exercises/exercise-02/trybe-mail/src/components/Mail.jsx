1d788d64868ed074c5351bdd089c2a88e25e879a
vitoraok1 <vitoraoki17@gmail.com>
2023-02-26 11:05:24 -0300

    creating the function to change read/unread mails using hook useState

--- section-09-react-hooks-and-context-api/day-01-react-context-api/fixation-exercises/exercise-02/trybe-mail/src/components/Mail.jsx
+++ section-09-react-hooks-and-context-api/day-01-react-context-api/fixation-exercises/exercise-02/trybe-mail/src/components/Mail.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './Mail.css';
 
 export default function Mail() {
   const mails = [
@@ -17,16 +18,49 @@ export default function Mail() {
       title: "Newsletter Omelete",
       status: 0
     },
-  ]
+  ];
+
+  const [mailStatus, setMailStatus] = useState(mails);
+
+  const changeAllRead = () => {
+
+    const allRead = mails.map((mail) => ({ ...mail, status: 1 }));
+
+    setMailStatus(allRead);
+
+  };
+
+  const changeAllUnread = () => {
+
+    const allUnread = mails.map((mail) => ({ ...mail, status: 0 }));
+
+    setMailStatus(allUnread);
+
+  };
+
+  const changeMail = (id, status) => {
+
+    const messages = mailStatus.map((mail) => {
+
+      if (mail.id === id) {
+        return { ...mail, status: status };
+      }
+      return mail;
+    });
+
+    setMailStatus(messages);
+
+  }
+
   return (
     <div>
-      <button type="button">Marcar todos como lido</button>
-      <button type="button">Marcar todos como não lido</button>
-      { mails.map((mail) => (
-        <div>
+      <button type="button" onClick={ changeAllRead }>Marcar todos como lido</button>
+      <button type="button" onClick={ changeAllUnread }>Marcar todos como não lido</button>
+      { mailStatus.map((mail) => (
+        <div id={ mail.id } key={ mail.id } className={ mail.status ? 'lido' : 'não-lido' }>
           <p>{ mail.title }</p>
-          <button type="button">Marcar como lido</button>
-          <button type="button">Marcar como não lido</button>
+          <button type="button" onClick={ () => changeMail(mail.id, 1) } >Marcar como lido</button>
+          <button type="button" onClick={ () => changeMail(mail.id, 0) }>Marcar como não lido</button>
         </div>
       ))}
     </div>

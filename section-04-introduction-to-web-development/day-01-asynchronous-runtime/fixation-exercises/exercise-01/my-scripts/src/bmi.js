209fc749d3f204c54a7a8457d06e1d3c8314f60f
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-10 19:10:27 -0300

    creating the logic to return which weight category the user is in from exercise 05

--- section-04-introduction-to-web-development/day-01-asynchronous-runtime/fixation-exercises/exercise-01/my-scripts/src/bmi.js
+++ section-04-introduction-to-web-development/day-01-asynchronous-runtime/fixation-exercises/exercise-01/my-scripts/src/bmi.js
@@ -1,7 +1,38 @@
 const readline = require('readline-sync');
 
-const weightInKg = readline.questionFloat('Whats your weight in kilograms?');
-const heightInCm = readline.questionInt('Whats your height in centimeters?');
+const BMI_MAX_AND_MIN = {
+
+  'Underweight': {
+    minBMI: 0,
+    maxBMI: 18.4,
+  },
+
+  'Normal Weight': {
+    minBMI: 18.5,
+    maxBMI: 24.9,
+  },
+
+  'Overweight': {
+    minBMI: 25,
+    maxBMI: 29.9,
+  },
+
+  'Obese Class I': {
+    minBMI: 30.0,
+    maxBMI: 34.9,
+  },
+
+  'Obese Class II': {
+    minBMI: 35,
+    maxBMI: 39.9,
+  },
+
+  'Obese Class III': {
+    minBMI: 40,
+    maxBMI: 100, 
+  },
+
+};
 
 function imcCalc(weight, height) {
   console.log(`Weight: ${weight}kg, Height: ${height}cm`);
@@ -12,10 +43,25 @@ function imcCalc(weight, height) {
   return imc;
 };
 
-function main() {
-  const imcFinal = imcCalc(weightInKg, heightInCm);
+function imcSituation(imc) {
+  const statuses = Object.keys(BMI_MAX_AND_MIN);
 
-  console.log(`Your IMC: ${imcFinal.toFixed(2)}`);
+  const resultFind = statuses.find((status) => {
+    const { maxBMI, minBMI } = BMI_MAX_AND_MIN[status];
+    return imc >= minBMI && imc <= maxBMI;
+  });
+
+  return resultFind;
+}
+
+function main() {
+  const weightInKg = readline.questionFloat('Whats your weight in kilograms?');
+  const heightInCm = readline.questionInt('Whats your height in centimeters?');
+
+  const imcFinal = imcCalc(weightInKg, heightInCm);
+  const situation = imcSituation(imcFinal);
+  
+  console.log(`Your IMC: ${imcFinal.toFixed(2)}, Your Situation: ${situation}`);
 };
 
 main();

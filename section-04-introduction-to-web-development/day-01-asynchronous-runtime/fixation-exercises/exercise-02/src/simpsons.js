92fd8ef3e13339a844842c6cc90d5e8a89ec459f
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-10 21:43:35 -0300

    exercise 02 - A

--- section-04-introduction-to-web-development/day-01-asynchronous-runtime/fixation-exercises/exercise-02/src/simpsons.js
+++ section-04-introduction-to-web-development/day-01-asynchronous-runtime/fixation-exercises/exercise-02/src/simpsons.js
@@ -1,1 +1,15 @@
 const fs = require('fs').promises;
+
+async function readAll() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const eachSimpson = simpsons.map(({ id, name }) => `${id} - ${name}`);
+
+  eachSimpson.forEach((nameAndId) => console.log(nameAndId));
+}
+
+async function main() {
+  await readAll();
+}
+
+main();

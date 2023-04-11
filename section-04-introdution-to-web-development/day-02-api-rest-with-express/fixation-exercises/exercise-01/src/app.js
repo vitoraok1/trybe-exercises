6b54741cc98e70638d3233bc99ddb766c0f21e2a
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-11 16:08:27 -0300

    creating JSON read function with fs module from exercise 01

--- section-04-introdution-to-web-development/day-02-api-rest-with-express/fixation-exercises/exercise-01/src/app.js
+++ section-04-introdution-to-web-development/day-02-api-rest-with-express/fixation-exercises/exercise-01/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
+
+const moviesPath = path.resolve(_dirname, './movies.json');
+
+async function readFile() {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(`File cannot be read: ${error}`);
+  }
+}
 
 module.exports = app;

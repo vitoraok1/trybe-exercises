d6066611084f09b19f15c934c4a75135fdac8b3c
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-11 16:31:47 -0300

    creating a post endpoint to register a new movie on JSON from exercise 01

--- section-04-introdution-to-web-development/day-02-api-rest-with-express/fixation-exercises/exercise-01/src/app.js
+++ section-04-introdution-to-web-development/day-02-api-rest-with-express/fixation-exercises/exercise-01/src/app.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 
 const moviesPath = path.resolve(__dirname, './movies.json');
 
@@ -29,6 +30,23 @@ app.get('/movies', async (req, res) => {
   try {
     const movies = await readFile();
     res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id:movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies)
+    res.status(201).json(newMovie);
   } catch (error) {
     res.status(500).send({ message: error.message })
   }

4397cdcff17da955f75de3cbde0fb494434f01e7
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-10 21:51:06 -0300

    exercise 02 - B

--- section-04-introduction-to-web-development/day-01-asynchronous-runtime/fixation-exercises/exercise-02/src/simpsons.js
+++ section-04-introduction-to-web-development/day-01-asynchronous-runtime/fixation-exercises/exercise-02/src/simpsons.js
@@ -8,8 +8,23 @@ async function readAll() {
   eachSimpson.forEach((nameAndId) => console.log(nameAndId));
 }
 
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const findSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+  if (!findSimpson) {
+    throw new Error('id não encontrado');
+  }
+
+  return findSimpson;
+}
+
 async function main() {
-  await readAll();
+  // await readAll();
+  const answerId = 2;
+  const getSimpson = await getSimpsonById(answerId);
+  console.log(getSimpson);
 }
 
 main();

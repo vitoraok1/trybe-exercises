37e8568bbcb62e86618b71fa1b34cf7c49bb7a9c
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-13 19:21:37 -0300

    creating and exporting the connections pool file

---
+++ section-04-introduction-to-web-development/day-05-express-and-mysql/fixation-exercises/exercise-01/exercise-todo-list-manager/src/db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2/promise');
+
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: 'root',
+  database: 'todolistdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+
+module.exports = connection;

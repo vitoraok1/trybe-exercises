8d2bfe8ef99afaa703cd09b2f44411feffd96a6f
Vitor Aoki <vitoraoki17@gmail.com>
2023-05-23 09:47:55 -0300

    exercise 04 - creating updateItem function using generics

--- section-08-typescript/day-02-static-typing-and-generics/fixation-exercises/exercise-01/mr-scott-grocerys/src/functions.ts
+++ section-08-typescript/day-02-static-typing-and-generics/fixation-exercises/exercise-01/mr-scott-grocerys/src/functions.ts
@@ -1,3 +1,12 @@
 export function first<Type>(array:Type[]):Type {
   return array[0];
 }
+
+export function updateItem<Type>(array: Type[], index: number, updatedItem: Type): Type[] {
+  if (!array.some((_element, indexFromElement) => indexFromElement === index)) return array;
+
+  return array.map((element, indexFromElement) => {
+    if (indexFromElement === index) return updatedItem;
+    return element;
+  });
+}

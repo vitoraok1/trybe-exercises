baa3b2b8f4aab1b6277d4897f088c68cafca8ad0
Vitor Aoki <vitoraoki17@gmail.com>
2023-05-23 09:49:00 -0300

    exercise 06 - creating buildCartItem function fix

--- section-08-typescript/day-02-static-typing-and-generics/fixation-exercises/exercise-01/mr-scott-grocerys/src/functions.ts
+++ section-08-typescript/day-02-static-typing-and-generics/fixation-exercises/exercise-01/mr-scott-grocerys/src/functions.ts
@@ -1,3 +1,7 @@
+import { CartItem } from "./types/CartItem";
+import { Drink } from "./types/Drink";
+import { Food } from "./types/Food";
+
 export function first<Type>(array:Type[]):Type {
   return array[0];
 }
@@ -9,4 +13,13 @@ export function updateItem<Type>(array: Type[], index: number, updatedItem: Type
     if (indexFromElement === index) return updatedItem;
     return element;
   });
+}
+
+export function buildCartItem(item: Food | Drink, quantity:number): CartItem {
+  const { name, price } = item;
+  return ({
+    name,
+    price,
+    quantity,
+  }) as CartItem;
 }

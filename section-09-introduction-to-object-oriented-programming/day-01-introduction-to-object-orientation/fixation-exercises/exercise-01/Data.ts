58b2f63c7c6415316ed478742f76a1ea62d67c8f
Vitor Aoki <vitoraoki17@gmail.com>
2023-05-30 16:18:05 -0300

    requirement 06 - creating getMonthName function

--- section-09-introduction-to-object-oriented-programming/day-01-introduction-to-object-orientation/fixation-exercises/exercise-01/Data.ts
+++ section-09-introduction-to-object-oriented-programming/day-01-introduction-to-object-orientation/fixation-exercises/exercise-01/Data.ts
@@ -37,7 +37,42 @@ export default class Data {
   public set year(value: number) {
     this._year = value;
   }
+
+  getMonthName(): string {
+    const month = this.month;
+    let monthStr = '';
+
+    switch (month) {
+      case 1: monthStr = 'January'       
+        break;
+      case 2: monthStr = 'February'       
+        break;
+      case 3: monthStr = 'March'       
+        break;
+      case 4: monthStr = 'April'       
+        break;
+      case 5: monthStr = 'May'       
+        break;
+        case 7: monthStr = "July";
+        break;
+      case 8: monthStr = "August";
+        break;
+      case 9: monthStr = "September";
+        break;
+      case 10: monthStr = "October";
+        break;
+      case 11: monthStr = "November";
+        break;
+      case 12: monthStr = "December";
+        break;
+      default:
+        break;
+    }
+
+    return monthStr;
+  }
 }
 
 const date = new Data(17, 4, 1994);
 console.log(date);
+console.log('O mês é:', date.getMonthName());

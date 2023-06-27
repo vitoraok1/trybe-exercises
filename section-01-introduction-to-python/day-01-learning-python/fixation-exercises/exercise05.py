59c03ec324af5fdbf7523b881c33401b1965ba09
Vitor Aoki <vitoraoki17@gmail.com>
2023-06-27 18:23:01 -0300

    Exercise 05 - creating a function that returns how many cans of painting and the total price

---
+++ section-01-introduction-to-python/day-01-learning-python/fixation-exercises/exercise05.py
@@ -0,0 +1,14 @@
+import math
+
+
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = math.ceil(required_liters / 18)
+    return required_cans, required_cans * can_price
+
+
+print(
+    "Test paint_costs function -",
+    paint_costs(20),
+)

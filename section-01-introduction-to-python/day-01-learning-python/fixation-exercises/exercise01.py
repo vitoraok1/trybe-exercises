298a9e2dd31cdf4ca81077721f25b606f891bf3a
Vitor Aoki <vitoraoki17@gmail.com>
2023-06-27 16:11:11 -0300

    Exercise 01 - creating a function that takes two numbers and returns de bigger

---
+++ section-01-introduction-to-python/day-01-learning-python/fixation-exercises/exercise01.py
@@ -0,0 +1,8 @@
+def biggerNumber(number1, number2):
+    """Take two numbers and return the bigger"""
+    if number1 > number2:
+        return number1
+    return number2
+
+
+print("Test biggerNumber function -", biggerNumber(4, 6))
